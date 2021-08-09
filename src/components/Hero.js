3b76b731d108a8937b1f615905cdb4d6513610c1
Madstick <ziliangz@gmail.com>
2021-08-08 22:11:58 -0700

    Hero section updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,31 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import '../index.css'
+import React from "react";
+import { Button } from "react-bootstrap";
+import "../index.css";
 
 const Hero = () => {
-    return (
+  return (
+    <div className="hero-container position-relative text-center">
+      <h1 className="hero-title">RESTORATION & RENOVATION</h1>
+      <h2 className="hero-block-text">
+        Flood, Fire, Emergency? We’re here to help you restore and renovate, any
+        time, day or night.
+      </h2>
+    </div>
+  );
+};
 
-        <div className="
-  position-relative
-  overflow-hidden
-  p-3 p-md-5
-  m-md-3
-  text-left
-  bg-light
-">
-  <div className="col-md-5 p-lg-5 ml-1 my-5">
-    <h1 className="hero-title text-center">Restorate and Renovate your home</h1>
-    <p className="lead fs-5 ">
-      Flood, Fire, Emergency? We’re here to help you restore and renovate,
-      any time, day or night.
-    </p>
-    <Link className="btn btn-outline-secondary" to="/">Get a free quote </Link>
-  </div>
-  <div className="product-device shadow-sm d-none d-md-block" />
-  <div className="product-device product-device-2 shadow-sm d-none d-md-block" />
-</div>
-
-    )
-}
-
-export default Hero
+export default Hero;

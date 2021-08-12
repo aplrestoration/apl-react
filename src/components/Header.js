3645dfb86bf066bed935c71d2062b9ab925e654c
Madstick <ziliangz@gmail.com>
2021-08-12 01:50:42 -0700

    changed some styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,65 +3,70 @@ import { Link } from "react-router-dom";
 import MobileMenu from "./MobileMenu";
 
 function Header() {
-  const [show,setShow]=useState(true)
+  const [showMobile, setShow] = useState(true);
+  const [showService, setService] = useState(true);
 
   return (
-    <div className="header-container">
-      <div className="header-nav-container">
-        {/* Site Logo  */}
-        <div className="header-nav-logo d-flex justify-content-between">
-          <Link to="/" className="text-dark text-decoration-none">
-            <h3 className="site-logo">APL restoration</h3>
-          </Link>
-          <span className="header-nav-info d-none d-xl-block">
-            24/7 Emergency Toll-Free Number <br />
-            1-833-7GOT-FlOOD（1-833-746-8356)
-          </span>
-        </div>
+    <div>
+      <div className="header-container">
+        <div className="header-nav-container">
+          {/* Site Logo  */}
+          <div className="header-nav-logo d-flex justify-content-between">
+            <Link to="/" className="text-dark text-decoration-none">
+              <h3 className="site-logo">APL restoration</h3>
+            </Link>
+            <span className="header-nav-info d-none d-xl-block">
+              24/7 Emergency Toll-Free Number <br />
+              1-833-7GOT-FlOOD（1-833-746-8356)
+            </span>
+          </div>
 
-        {/* Desktop Navigation  */}
-        <div className="header-nav">
-          <ul className="nav d-none d-lg-flex">
-            <li className="nav-item dropdown">
-              <Link to="/" className="nav-link active" aria-current="page">
-                Service
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-link nav-custom">
-                Who We Serve
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-link nav-custom">
-                Where We Serve
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/about" className="nav-link nav-custom">
-                About
-              </Link>
-            </li>
-          </ul>
-        </div>
+          {/* Desktop Navigation  */}
+          <div className="header-nav">
+            <ul className="nav d-none d-lg-flex">
+              <li className="nav-item dropdown">
+                <Link to="/" className="nav-link active" aria-current="page">
+                  Service
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/" className="nav-link nav-custom">
+                  Who We Serve
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/" className="nav-link nav-custom">
+                  Where We Serve
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about" className="nav-link nav-custom">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-        {/* Mobile Hamburger Menu */}
-        <div className="header-nav-menu d-lg-none" onClick={()=>setShow(!show)} >
-          <Link to="/">
-            <svg viewBox="0 0 100 80" width="40" height="40">
-              <rect width="100" height="20"></rect>
-              <rect y="30" width="100" height="20"></rect>
-              <rect y="60" width="100" height="20"></rect>
-            </svg>
-          </Link>
-          
+          {/* Mobile Hamburger Menu */}
+          <div
+            className="header-nav-menu d-lg-none"
+            onClick={() => setShow(!showMobile)}
+          >
+            <Link to="/">
+              <svg viewBox="0 0 100 80" width="40" height="40">
+                <rect width="100" height="20"></rect>
+                <rect y="30" width="100" height="20"></rect>
+                <rect y="60" width="100" height="20"></rect>
+              </svg>
+            </Link>
+          </div>
         </div>
       </div>
 
       {/* Mobile Navigation Panel  */}
-      {show ? <MobileMenu />: null}
+      {showMobile ? <MobileMenu /> : null}
     </div>
   );
-};
+}
 
 export default Header;
--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -2,13 +2,11 @@ import React from "react";
 
 const MobileMenu = () => {
   return (
-    <div>
       <div className="mobile-menu d-lg-none">
         <div className="mobile-menu-card">
           <p>Test: This is mobile menu </p>
         </div>
       </div>
-    </div>
   );
 };
 

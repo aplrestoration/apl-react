2305fa4fd651176ad8834d6ec069c18b94892750
Madstick <ziliangz@gmail.com>
2021-08-18 15:15:29 -0700

    about page added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
 
 const About = () => {
-    return (
-        <div>
-            <h1>This is About </h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="container-fluid hero-container position-relative">
+          <Container>
+          <h1 className="page-title">About Us</h1>
+          </Container>
+      </div>
+      
+      <Container>
+        <p>
+          The management team of APL Restoration has over 20 years’ experience
+          in helping people who have experienced a flood, or sewer back up. We
+          also have extensive experience with the insurance industry and we are
+          familiar with their different requirements. This knowledge often makes
+          all the difference in a claim proceeding smoothly.
+        </p>
+        <p>
+          At APL Restoration we make use of the latest technology. We use an IR
+          Camera that allows us to see through walls and floors to check for the
+          source of leaks and water damage. This is just one way in which we use
+          technology to address water problems. We also work with an extensive
+          team of plumbers, drain professionals, flood technicians, and other
+          important types of skilled tradesmen.
+        </p>
+        <h1>24 Hour Emergency Service:</h1>
+        <ul>
+          <li>Free on-site inspection to help determine coverage</li>
+          <li>Exceptional customer service</li>
+          <li>Complete property restoration-Free estimates</li>
+        </ul>
+      </Container>
+    </div>
+  );
+};
 
-export default About
+export default About;

9c1ffac27046c0dd48432ec1cf0936596ba842f4
Madstick <ziliangz@gmail.com>
2021-08-23 10:37:49 -0700

    /

--- src/components/SmallGallery.js
+++ src/components/SmallGallery.js
@@ -13,7 +13,7 @@ const SmallGallery = () => {
     return (
         <div className="container px-4 py-5" id="custom-cards">
         <h2 className="pb-2 big-title-2">RENOVATION SERVICES</h2>
-        <div className="row row-cols-2 row-cols-lg-4 align-items-stretch g-4 pt-5">
+        <div className="row row-cols-2 row-cols-lg-4 align-items-stretch g-4">
           <div className="col">
             <div className="
               card card-cover

3998144616eac5ed7bafd99ad88abbd8bd27f82d
Jason Allen <jasona@breezemaxweb.com>
2022-02-18 16:56:18 -0500

    bmw-meta tags update

--- src/SeoTags.js
+++ src/SeoTags.js
@@ -8,6 +8,156 @@ const seoTags = {
                 "content": "https://www.aplrestoration.ca/"
             }
         ]
+    },
+    "/drywall-service": {
+        "title": "Drywall Repair | Fire Damage - APL Restoration Vancouver",
+        "description": "The team here at APL Restoration Ltd has a lot of expertise in drywall repairs. We will patch-up and repaint your damaged drywall. Call us today!",
+        "meta": [
+            {
+                "property": "og:url",
+                "content": "https://www.aplrestoration.ca/drywall-service"
+            }
+        ]
+    },
+    "/emergency-response": {
+        "title": "Emergency Response | Water Damage | Fire Damage | APL",
+        "description": "Whatever the cause, APL Restoration professional damage restoration services are available 365 days a year, 24 hours a day to respond to an emergency.",
+        "meta": [
+            {
+                "property": "og:url",
+                "content": "https://www.aplrestoration.ca/emergency-respose"
+            }
+        ]
+    },
+    "/door-and-trimworks": {
+        "title": "Door & Trim Works | Flooring Installation Vancouver | APL",
+        "description": "APL Restoration provide you with a full range of doors and trims services. Call us today for basic baseboards to window castings and crown mouldings.",
+        "meta": [
+            {
+                "property": "og:url",
+                "content": "https://www.aplrestoration.ca/door-and-trimworks"
+            }
+        ]
+    },
+    "/floor-installation": {
+        "title": "Floor Installation Vancouver | HardWood | Laminate | Carpeting",
+        "description": "APL Restoration Ltd has an extensive inventory of flooring options in all types, colors, shapes and sizes. Contact us today for Floor Installation!",
+        "meta": [
+            {
+                "property": "og:url",
+                "content": "https://www.aplrestoration.ca/floor-installation"
+            }
+        ]
+    },
+    "/tiling": {
+        "title": "Tiling | Restoration Services | APL Restoration Vancouver",
+        "description": "APL Restoration have a qualified tiler with more than 15 years’ experience in tiling service. Contact us today for more information.",
+        "meta": [
+            {
+                "property": "og:url",
+                "content": "https://www.aplrestoration.ca/tiling"
+            }
+        ]
+    },
+    "/painting": {
+        "title": "Painting | Drywall Repair | House Repaint | Apartment Repaint ",
+        "description": "APL Restoration Ltd helps you transform your home with color. Our professional painters can paint houses and apartments of all shapes and sizes, interior and exterior.",
+        "meta": [
+            {
+                "property": "og:url",
+                "content": "https://www.aplrestoration.ca/painting"
+            }
+        ]
+    },
+    "/basement-finishing": {
+        "title": "Professional Basement Finishing |  Flooring Installation | APL",
+        "description": "Are you looking to build out your dream basement?  We offer professional basement finishing services to homeowners in Greater Vancouver.",
+        "meta": [
+            {
+                "property": "og:url",
+                "content": "https://www.aplrestoration.ca/basement-finishing"
+            }
+        ]
+    },
+    "/demolition": {
+        "title": "Demolition | Fire Damage Restoration | APL Vancouver",
+        "description": "Environmental hazards? Demolition needed? APL Restoration is the right choice. We look forward to covering all your demolition needs.",
+        "meta": [
+            {
+                "property": "og:url",
+                "content": "https://www.aplrestoration.ca/demolition"
+            }
+        ]
+    },
+    "/bathroom-finishing": {
+        "title": "Bathroom Finishing | Drywall Repair | Tile Replacement | APL",
+        "description": "Make the bathroom your favorite room in the house. APL Restoration Ltd will help you create your dream bathroom from start to finish.",
+        "meta": [
+            {
+                "property": "og:url",
+                "content": "https://www.aplrestoration.ca/bathroom-finishing"
+            }
+        ]
+    },
+    "/water-and-flood": {
+        "title": "Water Damage Restoration Vancouver | Available 24/7 | APL",
+        "description": "As soon as you experience water damage in your home or business, call APL Restoration. Quick action is necessary.",
+        "meta": [
+            {
+                "property": "og:url",
+                "content": "https://www.aplrestoration.ca/water-and-flood"
+            }
+        ]
+    },
+    "/biohazard-redemption": {
+        "title": "Biohazard Redemption | Mould | Damp | APL ",
+        "description": " If you find that your home or business has begun to show signs of mould damage, it’s important to act as quickly as possible.",
+        "meta": [
+            {
+                "property": "og:url",
+                "content": "https://www.aplrestoration.ca/biohazard-redmemption"
+            }
+        ]
+    },
+    "/fire-and-smoke": {
+        "title": "Fire & Smoke | Fire Damange Restoration Vancouver",
+        "description": "Fires can do more than compromise the structural integrity of your building. Contact us today for fire damage restoration.",
+        "meta": [
+            {
+                "property": "og:url",
+                "content": "https://www.aplrestoration.ca/fire-and-smoke"
+            }
+        ]
+    },
+    "/who-we-serve": {
+        "title": "Who We Serve | Drywall Repair Vancouver",
+        "description": "At APL, we’re skilled team with experience of handle renovation and restoration of all kinds.",
+        "meta": [
+            {
+                "property": "og:url",
+                "content": "https://www.aplrestoration.ca/who-we-serve"
+            }
+        ]
+    },
+    "/where-we-serve": {
+        "title": "Where We Serve | Greater Vancouver | Surrey",
+        "description": "As soon as you experience water damage in your home or business, call APL Restoration. Quick action is necessary.",
+        "meta": [
+            {
+                "property": "og:url",
+                "content": "https://www.aplrestoration.ca/where-we-serve"
+            }
+        ]
+    },
+    "/about": {
+        "title": "Abous Us | Restoration Company | Drywall Repair",
+        "description": "The management team of APL Restoration has over 20 years’ experience in helping people who have experienced a flood, or sewer back up.",
+        "meta": [
+            {
+                "property": "og:url",
+                "content": "https://www.aplrestoration.ca/about"
+            }
+        ]
     }
 }
 
